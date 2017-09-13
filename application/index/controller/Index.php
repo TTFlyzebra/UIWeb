2c6330d9409503e7d280d34ab8b13a2977ec6fea
flyzebra <852184385@qq.com>
2017-09-13 11:47:43 +0800

    配置资源目录

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -3,16 +3,9 @@
 namespace app\index\controller;
 
 use think\Controller;
-use think\Db;// 引入Controller类
 
 class Index extends Controller {
 	public function index() {
-        $name = "UI管理模块";
-		$this->assign('name',$name);
-        $cell	=	Db::name('cell')->find();
-		$this->assign('cell',$cell);
 		return $this->fetch ();
-
-
 	}
 }
--- public/index.php
+++ public/index.php
@@ -13,5 +13,10 @@
 
 // 定义应用目录
 define('APP_PATH', __DIR__ . '/../application/');
+
+
+//定义路径变量
+
+define('_CSS',__DIR__.'./static/css');
 // 加载框架引导文件
 require __DIR__ . '/../thinkphp/start.php';
