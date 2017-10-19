02dbf2f38a4a47a5ed6d9a88f77fb8d21cab7071
flyzebra <852184385@qq.com>
2017-10-19 16:14:17 +0800

    配置cell页面开发

--- public/static/js/flyscreen.js
+++ public/static/js/flyscreen.js
@@ -1,6 +1,8 @@
-var screenArr = [];
-var cellMenuArr = [];
-var moveCell;
+var screenCellArr = [];
+var menuCellArr = [];
+var moveCell = null;
+var moveDiv = null;
+var addCell = null;
 
 /**
  * 将以"px"字符串结尾的字符串转换成int整数
@@ -12,6 +14,7 @@ function trimPX(_px) {
         return 0;
     return parseInt(_px.substr(0, _px.lastIndexOf("px")));
 }
+
 
 /**
  * @param cell
@@ -54,13 +57,15 @@ function cellDiv(cell) {
                 }
                 text.get(0).innerHTML = content;
             } catch (e) {
-                text.get(0).innerHTML = e;
+                text.get(0).innerHTML = '';
+                console.log(e);
             }
             div.get(0).append(text.get(0));
             break;
     }
-    div.on('click', function (event) {
-        alert('x=' + event.clientX);
+    div.on('mousedown', function (event) {
+        moveCell = cell;
+
     });
     return div.get(0);
 }
@@ -102,8 +107,9 @@ function cellMenuDiv(cell) {
             div.get(0).append(image.get(0));
             break;
     }
-    div.on('click', function (event) {
+    div.on('mousedown', function (event) {
         //添加点击事件
+        addCell = cell;
     });
     return div.get(0);
 }
@@ -114,8 +120,12 @@ function CellMenu(cell) {
     this.cellMenuDiv = cellMenuDiv(cell);
 }
 
-
+/**
+ * 根据TabID获取一页的cell数据
+ * @param tabId
+ */
 function getScreenCell(tabId) {
+    var flyscreen = $('.flyscreen').get(0);
     $.ajax({
         url: testurl,
         type: "get",
@@ -124,11 +134,11 @@ function getScreenCell(tabId) {
         success: function (result) {
             try {
                 var data = JSON.parse(result);
-                screenArr.splice(0, screenArr.length);
-                $('#flyscreen').get(0).innerHTML = "";
+                screenCellArr.splice(0, screenCellArr.length);
+                flyscreen.innerHTML = '';
                 for (i = 0; i < data.cellList.length; i++) {
-                    screenArr[i] = new Cell(data.cellList[i]);
-                    $('#flyscreen').get(0).append(screenArr[i].cellDiv);
+                    screenCellArr[i] = new Cell(data.cellList[i]);
+                    flyscreen.append(screenCellArr[i].cellDiv);
                 }
             } catch (e) {
                 console.log(e);
@@ -137,7 +147,11 @@ function getScreenCell(tabId) {
     });
 }
 
+/**
+ * 获取添加的所有cell样板数据
+ */
 function getCellMenu() {
+    var flycellmenu = $('.flycellmenu').get(0);
     $.ajax({
         url: cellurl,
         type: "get",
@@ -146,14 +160,14 @@ function getCellMenu() {
         success: function (result) {
             try {
                 var data = JSON.parse(result);
-                cellMenuArr.splice(0, cellMenuArr.length);
-                $('#flycellmenu').get(0).innerHTML = "";
+                menuCellArr.splice(0, menuCellArr.length);
+                flycellmenu.innerHTML = '';
                 for (i = 0; i < data.rows.length; i++) {
-                    cellMenuArr[i] = new CellMenu(data.rows[i]);
-                    $('#flycellmenu').get(0).append(cellMenuArr[i].cellMenuDiv);
+                    var num = screenCellArr.length;
+                    menuCellArr[num] = new CellMenu(data.rows[i]);
+                    flycellmenu.append(menuCellArr[num].cellMenuDiv);
                 }
             } catch (e) {
-                alert(e);
                 console.log(e);
             }
         }
@@ -162,4 +176,59 @@ function getCellMenu() {
 
 +$(function () {
     getCellMenu();
+    $(document.body).on('mousedown', function (event) {
+        //
+    }).on('mousemove', function (event) {
+        try {
+            if (addCell !== null) {
+                addCell.imgUrl = addCell.imageurl1;
+                if (moveDiv === null) {
+                    moveDiv = cellDiv(addCell);
+                    this.append(moveDiv);
+                }
+                moveDiv.style.left = event.clientX + 'px';
+                moveDiv.style.top = event.clientY + 'px';
+            }
+        } catch (e) {
+            alert(e);
+        }
+    }).on('mouseup', function (event) {
+        if (moveDiv !== null) {
+            this.removeChild(moveDiv);
+            moveDiv = null;
+        }
+
+        var flyscreen = $('.flyscreen').get(0);
+
+        var left = flyscreen.offsetLeft;
+        var top = flyscreen.offsetTop;
+        var width = flyscreen.offsetWidth;
+        var height = flyscreen.offsetHeight;
+
+        if (event.clientX >= left && event.clientX <= (width+left) && event.clientY >= top && event.clientY <= (height+top)) {
+            if (addCell !== null) {
+                var x = event.clientX - left + flyscreen.scrollLeft;
+                var y = event.clientY - top + flyscreen.scrollTop;
+                addCell.x = x * 1920 / 1280;
+                addCell.y = y * 1920 / 1280;
+                addCell.imgUrl = addCell.imageurl1;
+                var num = screenCellArr.length;
+                screenCellArr[num] = new Cell(addCell);
+                flyscreen.append(screenCellArr[num].cellDiv);
+            }
+        }
+
+        if (addCell !== null) {
+            addCell = null;
+        }
+    });
+
+    $('.flyscreen').on('mousemove', function (event) {
+        var left = this.offsetLeft;
+        var top = this.offsetTop;
+        var x = event.clientX - left + this.scrollLeft;
+        var y = event.clientY - top + this.scrollTop;
+        $('.flyscreenmsg').get(0).innerHTML = x + '-' + y;
+    }).on('mouseup', function (event) {
+    });
 });
