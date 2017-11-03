37607237467b2266c046aec353e817ccd59779fb
flyzebra <852184385@qq.com>
2017-11-03 17:01:04 +0800

    db.sql置为空表

--- db.sql
+++ db.sql
@@ -65,7 +65,6 @@ CREATE TABLE `fly_cell` (
 
 LOCK TABLES `fly_cell` WRITE;
 /*!40000 ALTER TABLE `fly_cell` DISABLE KEYS */;
-INSERT INTO `fly_cell` VALUES (1,75,'',198,195,'/myweb/ui/public/uploads/98/5da12c1afcb2b7bfdc386b7fb2ddbe.png','','',0,'',0,NULL,NULL,1,255,0,0,0,0,0,NULL,NULL,NULL,NULL,NULL,NULL,'2017-10-30 09:30:31','2017-10-30 09:30:31',0,'0.0.0.0'),(2,75,'',229,220,'/myweb/ui/public/uploads/1c/9bb05df498a27f2170a4f43f64b849.png','','',0,'',0,NULL,NULL,1,255,0,0,0,0,0,NULL,NULL,NULL,NULL,NULL,NULL,'2017-11-01 17:12:10','2017-11-01 17:12:10',0,'0.0.0.0'),(3,75,'',426,585,'/myweb/ui/public/uploads/2d/aabc960f1fca0996937cc634c0e0d4.png','','',0,'',0,NULL,NULL,1,255,0,0,0,0,0,NULL,NULL,NULL,NULL,NULL,NULL,'2017-11-02 11:39:35','2017-11-02 11:39:35',0,'0.0.0.0');
 /*!40000 ALTER TABLE `fly_cell` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -102,7 +101,6 @@ CREATE TABLE `fly_celltype` (
 
 LOCK TABLES `fly_celltype` WRITE;
 /*!40000 ALTER TABLE `fly_celltype` DISABLE KEYS */;
-INSERT INTO `fly_celltype` VALUES (75,1,'图片文本','/myweb/ui/public/uploads/98/5da12c1afcb2b7bfdc386b7fb2ddbe.png','','','','','可以同时显示图片和文本的控件。','2017-10-24 09:34:19','2017-10-24 09:34:19',0,'0.0.0.0'),(76,2,'视频播放','/myweb/ui/public/uploads/9a/3826de18a58cdd7fcb5c0c42abd2d8.gif','播放地址','播放接口','循环播放','','小视频窗口控件，播放地址指定视频流位置，播放接口指定获取一组视频流的地址(接口数据格式为JSON{name:\"\";url:\"\";msg:\"\";ret:\"\"})。','2017-10-24 09:43:24','2017-10-24 09:43:24',0,'0.0.0.0'),(81,3,'轮播控件','/myweb/ui/public/uploads/39/5335689f562aeadff38c3b743f6960.png','图片列表','','','','','2017-10-24 09:53:11','2017-10-24 09:53:11',0,'0.0.0.0'),(86,4,'组合控件','/myweb/ui/public/uploads/2d/aabc960f1fca0996937cc634c0e0d4.png','','','','','周围可以弹出6个展示图片的控件。','2017-11-02 11:49:00','2017-11-02 11:49:00',0,'0.0.0.0');
 /*!40000 ALTER TABLE `fly_celltype` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -162,7 +160,6 @@ CREATE TABLE `fly_table` (
 
 LOCK TABLES `fly_table` WRITE;
 /*!40000 ALTER TABLE `fly_table` DISABLE KEYS */;
-INSERT INTO `fly_table` VALUES (2,'主页面','/myweb/ui/public/uploads/13/bb3a0260c78cba6b0cd071f39a5a9c.png','','2017-11-02 16:39:24','2017-11-02 16:39:24',0,'0.0.0.0'),(3,'陕西TV','','','2017-11-02 17:55:23','2017-11-02 17:55:23',0,'0.0.0.0');
 /*!40000 ALTER TABLE `fly_table` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -180,7 +177,9 @@ CREATE TABLE `fly_tablecell` (
   `y` int(11) NOT NULL,
   `width` int(11) NOT NULL,
   `height` int(11) NOT NULL,
-  `remark` text
+  `remark` text,
+  KEY `tableId` (`tableId`),
+  KEY `cellId` (`cellId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -190,7 +189,6 @@ CREATE TABLE `fly_tablecell` (
 
 LOCK TABLES `fly_tablecell` WRITE;
 /*!40000 ALTER TABLE `fly_tablecell` DISABLE KEYS */;
-INSERT INTO `fly_tablecell` VALUES (3,2,769,532,229,220,NULL),(3,1,1171,397,198,195,NULL),(3,3,214,183,426,585,NULL),(3,1,1171,397,198,195,NULL),(3,2,769,532,229,220,NULL),(2,2,1540,160,229,220,NULL),(2,1,1177,454,198,195,NULL),(2,2,1540,160,229,220,NULL),(2,2,454,460,229,220,NULL);
 /*!40000 ALTER TABLE `fly_tablecell` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -264,4 +262,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-11-03 13:56:11
+-- Dump completed on 2017-11-03 16:59:57
