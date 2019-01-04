2b6d26972fe516dab3e3e8c17bd47aabfa0d0bc9
flycnzebra <tang.shiyuan@jancar.com.cn>
2019-01-04 16:33:27 +0800

    .........

--- app/config.php
+++ app/config.php
@@ -132,10 +132,10 @@ return [
 
     // 视图输出字符串内容替换
     'view_replace_str'       => [
-        '_CSS_'=>'/myweb/ui/public/static/css',
-        '_JS_'=>'/myweb/ui/public/static/js',
-        '_IMG_'=>'/myweb/ui/public/static/img',
-        '_FT_'=>'/myweb/ui/public/static/fonts'
+        '_CSS_'=>'/flyme/public/static/css',
+        '_JS_'=>'/flyme/public/static/js',
+        '_IMG_'=>'flyme/public/static/img',
+        '_FT_'=>'/flyme/public/static/fonts'
     ],
     // 默认跳转页面对应的模板文件
     'dispatch_success_tmpl'  => THINK_PATH . 'tpl' . DS . 'dispatch_jump.tpl',
