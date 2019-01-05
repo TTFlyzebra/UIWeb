d4de6bbf7c1bdd8ecd01182a1abe089ab91018d6
flycnzebra <tang.shiyuan@jancar.com.cn>
2019-01-05 09:03:53 +0800

    测试API

--- app/api/controller/Index.php
+++ app/api/controller/Index.php
@@ -5,15 +5,16 @@ class Index
 {
     public function index()
     {
-        $curl = curl_init();
-        curl_setopt($curl, CURLOPT_URL, 'http://192.168.1.43:9020/api/ui-operation/api/v/launcher_cell.json?tabId=162&token=');
-        curl_setopt($curl, CURLOPT_HEADER, FALSE);    //表示需要response header
-        curl_setopt($curl, CURLOPT_NOBODY, FALSE); //表
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
-        $data = curl_exec($curl);
-        curl_close($curl);
+//        $curl = curl_init();
+//        curl_setopt($curl, CURLOPT_URL, 'http://192.168.1.43:9020/api/ui-operation/api/v/launcher_cell.json?tabId=162&token=');
+//        curl_setopt($curl, CURLOPT_HEADER, FALSE);    //表示需要response header
+//        curl_setopt($curl, CURLOPT_NOBODY, FALSE); //表
+//        curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+//        $data = curl_exec($curl);
+//        curl_close($curl);
 //        $obj=json_decode($data);
 //        echo json_encode($obj);
-        echo $data;
+//        echo $data;
+        echo "api test";
     }
 }
