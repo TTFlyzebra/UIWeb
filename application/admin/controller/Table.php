5ca9096844062d5d4a9e568ddc9d52cfd0bfb5f9
flycnzebra <tang.shiyuan@jancar.com.cn>
2019-02-18 18:28:08 +0800

    table修改

--- application/admin/controller/Table.php
+++ application/admin/controller/Table.php
@@ -3,9 +3,27 @@
 namespace app\admin\controller;
 
 use think\Controller;
+use think\Db;
+use think\Request;
 
 class Table extends Controller {
 	public function index() {
 		return $this->fetch ();
 	}
+
+    public function add()
+    {
+        return $this->fetch();
+    }
+
+    public function edit()
+    {
+        $request = Request::instance();
+        if ($request->has('id', 'get')) {
+            $db = Db::name('table');
+            $item = $db->where('tableId', $_GET['id'])->find();
+            $this->assign('item', $item);
+        }
+        return $this->fetch();
+    }
 }
