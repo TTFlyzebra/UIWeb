b543841ad237f6a8c88c90384821889edf090112
flycnzebra <tang.shiyuan@jancar.com.cn>
2019-02-21 18:34:24 +0800

    newsdetail

--- application/index/controller/News.php
+++ application/index/controller/News.php
@@ -3,9 +3,21 @@
 namespace app\index\controller;
 
 use think\Controller;
+use think\Db;
+use think\Request;
 
 class News extends Controller {
 	public function index() {
 		return $this->fetch ();
 	}
+	public function detail(){
+        $request = Request::instance();
+        if ($request->has('id', 'get')) {
+            $db = Db::name('news');
+            $item = $db->where('newsId', $_GET['id'])->find();
+            $this->assign('item', $item);
+            return $this->fetch ();
+        }
+        return $this->fetch ();
+    }
 }
