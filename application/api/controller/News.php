5a835d228f0df2e10c47a8ecc991b9eaab18600e
flycnzebra <tang.shiyuan@jancar.com.cn>
2019-02-21 18:13:57 +0800

    news admin add edit delete

---
+++ application/api/controller/News.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace app\api\controller;
+
+use think\Db;
+use think\Request;
+
+class News
+{
+    public function index()
+    {
+        $request = Request::instance();
+        if ($request->isDelete()) {
+            $delnewsId = ($request->only('newsId'))['newsId'];
+            $result = Db::name("news")->where('newsId', $delnewsId)->delete();
+        } elseif ($request->isPut()) {
+            $news = $request->put();
+            $news['ip'] = request()->ip();
+            $result = Db::name("news")->update($news);
+        } elseif ($request->isPost()) {
+            $news = $request->post();;
+            $news['ip'] = request()->ip();
+            $result = Db::name("news")->insert($news);
+        } elseif ($request->isGet()) {
+            $db = Db::name("news");
+            $db->order('newsSort asc');
+            if ($request->has('limit', 'get') && $request->has('offset', 'get')) {
+                $db->limit($_GET['offset'], $_GET['limit']);
+            }
+            $db->field('userid,ip', true);
+            $newss = $db->select();
+            if ($request->isAjax()) {
+                $resultdata['total'] = $db->count();
+                $resultdata['rows'] = $newss;
+                echo json_encode($resultdata);
+            } else {
+                echo json_encode($newss);
+            }
+        }
+    }
+
+}
