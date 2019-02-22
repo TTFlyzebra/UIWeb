de82c9bf08f0510e9560e9270868688a939ab45e
flycnzebra <tang.shiyuan@jancar.com.cn>
2019-02-22 14:13:33 +0800

    news bug

--- application/index/controller/News.php
+++ application/index/controller/News.php
@@ -6,18 +6,38 @@ use think\Controller;
 use think\Db;
 use think\Request;
 
-class News extends Controller {
-	public function index() {
-		return $this->fetch ();
-	}
-	public function detail(){
+class News extends Controller
+{
+    public function index()
+    {
+        $request = Request::instance();
+        $db = Db::name('news');
+        $page = array();
+        $page['limit'] = 6;
+        $page['curr'] = 1;
+        $page['count'] = $db->count();
+        if ($request->has('limit', 'get') && $request->has('offset', 'get')) {
+            $page['curr'] = $_GET['offset'];
+            $page['limit'] = $_GET['limit'];
+        }
+        $db->limit(($page['curr'] - 1) * $page['limit'], $page['limit']);
+        $newss = $db->select();
+        $this->assign('list1', $newss);
+        $this->assign('page', $page);
+        return $this->fetch();
+    }
+
+    public function detail()
+    {
         $request = Request::instance();
         if ($request->has('id', 'get')) {
             $db = Db::name('news');
             $item = $db->where('newsId', $_GET['id'])->find();
+            $item['newsText'] = preg_replace("/\r/\n", "<br/>", $item['newsText']);
+            $item['remark'] = preg_replace("/\r/\n", "<br/>", $item['remark']);
             $this->assign('item', $item);
-            return $this->fetch ();
+            return $this->fetch();
         }
-        return $this->fetch ();
+        return $this->fetch();
     }
 }
