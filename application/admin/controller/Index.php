d7cebdeccd0d0aeededc03b064389e9eaad33c5b
flycnzebra <tang.shiyuan@jancar.com.cn>
2019-02-23 15:10:47 +0800

    Auth test

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -3,7 +3,6 @@
 namespace app\admin\controller;
 
 use think\Controller;
-use think\Db;
 
 class Index extends Controller
 {
---
+++ application/admin/controller/Info.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace app\admin\controller;
+
+
+class Info extends Auth
+{
+    public function index()
+    {
+    }
+
+}
