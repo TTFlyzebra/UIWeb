bd9a7cf40e5717c620138a8f0ac74545a65fc9b1
flycnzebra <tang.shiyuan@jancar.com.cn>
2019-03-04 14:08:51 +0800

    login out

--- application/auth/controller/Login.php
+++ application/auth/controller/Login.php
@@ -16,7 +16,7 @@ class Login extends Controller
         $request = Request::instance();
         if ($request->has('loginname', 'post')&&$request->has('loginword', 'post')){
             if($_POST['loginname']=='admin'&&$_POST['loginword']==md5('123456')){
-                session('flyid',$_POST['loginname']);
+                session('user_id',$_POST['loginname']);
             }else{
                 echo "password error!";
             }
@@ -26,9 +26,8 @@ class Login extends Controller
     }
 
     public function logout(){
-        session_unset();
-        session_destroy();
-        $this->redirect("/auth/login/login");
+        session("user_id", NULL);
+        $this->redirect(url('auth/login/index'));
     }
 
 }
