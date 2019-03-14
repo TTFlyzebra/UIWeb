c0d1ad5d2acd39e106a613844fa8c7f88de54265
flycnzebra <tang.shiyuan@jancar.com.cn>
2019-03-14 09:56:28 +0800

    add subcell

--- application/flyui/controller/Cell.php
+++ application/flyui/controller/Cell.php
@@ -6,33 +6,42 @@ use app\auth\controller\Auth;
 use think\Db;
 use think\Request;
 
-class Cell extends Auth {
-	public function index() {
-	    $celltypes = Db::name('celltype')->select();
-	    $this->assign('list',$celltypes);
-		return $this->fetch ();
-	}
-
-	public function oldadd(){
-        $this->assign('list1',Db::name('celltype')->select());
-        return $this->fetch ();
+class Cell extends Auth
+{
+    public function index()
+    {
+        $celltypes = Db::name('celltype')->select();
+        $this->assign('list', $celltypes);
+        return $this->fetch();
     }
 
-    public function add(){
-        $this->assign('list1',Db::name('celltype')->select());
-        return $this->fetch ();
+    public function oldadd()
+    {
+        $this->assign('list1', Db::name('celltype')->select());
+        return $this->fetch();
     }
 
-    public function edit(){
-        $this->assign('list1',Db::name('celltype')->select());
+    public function add()
+    {
+        $this->assign('list1', Db::name('celltype')->select());
+        return $this->fetch();
+    }
+
+    public function edit()
+    {
+        $this->assign('list1', Db::name('celltype')->select());
 
         $request = Request::instance();
         if ($request->has('id', 'get')) {
             $item = Db::name('cell')->where('cellId', $_GET['id'])->find();
             $this->assign('item', $item);
             $subcells = Db::name('cellsub')->where('cellId', $_GET['id'])->select();
-            $this->assign('list2',$subcells);
-            return $this->fetch ();
+            $this->assign('list2', $subcells);
+            $this->assign('subnum', sizeof($subcells));
+        }else{
+            $this->assign('list2', []);
+            $this->assign('subnum', 0);
         }
+        return $this->fetch();
     }
 }
