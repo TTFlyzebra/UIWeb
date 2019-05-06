08fc5bbd9f74a763427d39c5f442800ae0bf8505
flyzebra <852184385@qq.com>
2019-05-06 23:04:08 +0800

    edit web ui

--- public/static/res/flyui/js/flyimage.js
+++ public/static/res/flyui/js/flyimage.js
@@ -22,7 +22,33 @@
         var options = this.options;
         if (options.showPreview) {
             self.css("display", "none");
-            var divparent = $('<div onclick="" style="display: flex;align-items:center;justify-content:center;overflow:hidden;width:' +
+            //
+            // var divurl = $('<div style="width: 100%;"><span style="line-height:30px;float: left;vertical-align:middle;">图片地址：</span></div>')
+            // var divauto1 = $('<div style="overflow:hidden" ></div>');
+            // var imageurl = $('<input style="width: 100%;height: 30px" type="text" name="imageurl[]" value="">');
+            // this.imageurl = imageurl;
+            // divauto1.append(imageurl);
+            // divurl.append(divauto1);
+            // self.after(divurl);
+            //
+            // var divwh = $('<div style="width: 100%"></div>');
+            // var divwidth = $('<div style="width: 50%;float: left"><span style="line-height:30px;float: left;vertical-align:middle;">宽：</span></div>')
+            // var divauto2 = $('<div style="overflow: hidden"></div>');
+            // var imagew = $('<input style="width: 100%;height: 30px" type="text" name="imagew[]" value="">');
+            // this.imagew = imagew;
+            // divauto2.append(imagew);
+            // divwidth.append(divauto2);
+            // divwh.append(divwidth);
+            // var divheight = $('<div style="width: 50%;float: left"><span style="line-height:30px;float: left;vertical-align:middle;">高：</span></div>')
+            // var divauto3 = $('<div style="overflow: hidden"></div>');
+            // var imageh = $('<input style="width: 100%;height: 30px" type="text" name="imageh[]" value="">');
+            // this.imageh = imageh;
+            // divauto3.append(imageh);
+            // divheight.append(divauto3);
+            // divwh.append(divheight);
+            // self.after(divwh);
+
+            var divparent = $('<div style="display: flex;align-items:center;justify-content:center;overflow:hidden;width:' +
                 options.width + ';height:' + options.height + ';background:#0f0f0f"></div>');
             divparent.on('click', function () {
                 self.click();
@@ -31,19 +57,26 @@
             divimg.attr('src', options.image);
             divparent.append(divimg);
             this.divimg = divimg;
+            self.after(divparent);
+
             var del = $('<div style="cursor:pointer;position:absolute;right:16px;top:1px;' +
                 'width: 28px;heidth:28px;font-size: 20px;color:#000000;background: #FFFFFF">&nbsp;X</div>');
             del.on("click", function (event) {
                 divimg.attr('src', "");
+                imageurl.val("");
+                imagew.val("");
+                imageh.val(obj.data.height);
                 self.trigger("del", event);
             });
-            self.after(divparent);
             self.after(del);
         }
     };
 
     FlyImage.prototype.initevent = function () {
         var divimg = this.divimg;
+        var imageurl = this.imageurl;
+        var imagew = this.imagew;
+        var imageh = this.imageh;
         var self = this.$flyimage;
         var options = this.options;
         if (options.autoup === true) {
@@ -57,11 +90,13 @@
                     contentType: false,
                     processData: false,
                     success: function (result) {
-                        self.trigger("success", result);
                         try {
                             var obj = JSON.parse(result);
                             if (obj.code === 0) {
                                 divimg.attr('src', obj.data.saveName);
+                                imageurl.val(obj.data.saveName);
+                                imagew.val(obj.data.width);
+                                imageh.val(obj.data.height);
                             } else {
                                 divimg.attr('src', options.image);
                                 alert(obj.msg + "data" + obj.data);
@@ -70,10 +105,11 @@
                             divimg.attr('src', options.image);
                             alert("catch error:" + e);
                         }
+                        self.trigger("success", result);
                     },
                     error: function (result) {
-                        alert("上传图片失败！");
                         divimg.attr('src', options.image);
+                        alert("上传图片失败！");
                     }
                 });
             });
